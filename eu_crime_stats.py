ee46f4bbb9abcb7cb8823404f5f7518d12c178ed
cormacio100 <cormac.music@gmail.com>
2017-04-12 00:02:13 +0100

    index template added. full bootstrap install added for glyphicons

--- eu_crime_stats.py
+++ eu_crime_stats.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

0d78b9f6fb3e9d58b236545d858b1413f6a85241
cormacio100 <cormac.music@gmail.com>
2017-04-19 20:49:26 +0100

    API Built and connected to successfully

--- eu_crime_stats.py
+++ eu_crime_stats.py
@@ -6,11 +6,50 @@ import os
 
 app = Flask(__name__)
 
+'''
+LOCAL DB SETTINGS
+'''
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'projectModule2'
+COLLECTION_NAME = 'eu_crime_stats'
+
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
+
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+
+
+@app.route('/charts/data')
+def charts_data():
+    """
+        :return: data from the database
+    """
+    #   CONSTANT that defines the record fields that we wish to retrieve
+    FIELDS = {
+        '_id': False,
+        'category':True,
+        'eu_member_state': True,
+        'year': True,
+        'amount': True
+    }
+
+    #   Open a connection to MongoDB
+    #   the "with" statement auto-closes the connection when done
+    with MongoClient(MONGODB_HOST,MONGODB_PORT) as conn:
+        #   Define the collection to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        #   Retrieve a result set that includes the properties
+        #   defined in FIELDS
+        eu_crime_stats = collection.find(projection=FIELDS,limit=1000)
+        #   convert the data to a list in a JSON object
+        #   and RETURN the JSONS
+        return json.dumps(list(eu_crime_stats))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/graph.js
+++ static/js/graph.js
@@ -1,3 +1,16 @@
 /**
  * Created by Cormac Liston on 11/04/2017.
  */
+
+//  RETRIEVE DATA FROM API
+queue()
+    .defer(d3.json,'/charts/data')
+    .await(buildGraphs);
+
+function buildGraphs(error,jsonData){
+    var eu_crime_stats = jsonData
+
+    eu_crime_stats.forEach(function(d){
+       console.log(d);
+    });
+}

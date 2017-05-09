a33779d0d513c5f6e7cf7b7b88489e4f965848cc
cormacio100 <cormac.music@gmail.com>
2017-05-09 19:22:29 +0100

    Horizontal Row Chart Added

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,22 +2,14 @@
  * Created by Cormac Liston on 08/05/2017.
  */
 
-/**
- * Function tidies up the text displayed on the select dropdown for Year
- */
-function changeYearSelectText(){
-    console.log('changing text');
-    var optionArr = $('#menu-select').children('select').children('option');
-    $(optionArr ).each(function(){
-        var txt = $(this).text();
-        if(txt!=="Select all"){
-            txt = txt.substring(0,4);
-        }
-        $(this).text(txt);
-    });
-}
-
 $(function() {
-    $('#menu-select').on('click',changeYearSelectText);
+    console.log('page loading');
+
+    $('#menu-select').children('select').on('change',function(){
+        console.log('selection made');
+        console.log( $(this).find(":selected").val() );
+        //yearDim.filterExact("2010");
+        //print_filter(indexedData);
+    });
 });
 
--- static/js/graph.js
+++ static/js/graph.js
@@ -4,8 +4,12 @@
 
 function print_filter(filter) {
     var f=eval(filter);
-    console.log(JSON.stringify(f));
+    if (typeof(f.length) != "undefined") {}else{}
+    if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
+    if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
+    console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
 }
+
 
 //  RETRIEVE DATA FROM API
 queue()
@@ -17,8 +21,6 @@ function buildGraphs(error,jsonData){
     var irishCrimeStats = [];
     euCrimeStats.forEach(function(d){
         if(d.eu_member_state == "Ireland"){
-           // console.log('FROM IRELAND');
-            //console.log(d);
             irishCrimeStats.push(d);
         }
     });
@@ -40,52 +42,59 @@ function buildGraphs(error,jsonData){
     var indexedData = crossfilter(irishCrimeStats);
 
     //#######################################################
-    //  2 - CREATE DIMENSIONS FOR FILTERING THE DATA
-    //  the X-AXIS
-
+    //  2 - CREATE DIMENSIONS FOR ACCESSING THE DATA
+    //  SELECT MENU
     var yearDim = indexedData.dimension(function(d){
        return d.year;
     });
-
-
-
+    //  HORIZONTAL BAR CHART
+    var amountOfCrimesDim = indexedData.dimension(function(d){
+        if(d.category=="crime") {
+            return d.type;
+        }
+    });
+    //  PIE CHART
     var crimeTypesDim = indexedData.dimension(function(d){
         if(d.category=="crime") {
             return d.type;
         }
     });
-
+    //  PIE CHART
     var justiceSystemTypesDim = indexedData.dimension(function(d){
         if(d.category=="justice_system") {
             return d.type;
         }
     });
 
+
+    //#######################################################
+    //  BAR CHART
+    //#######################################################
+
+
+
+
     //#######################################################
     //  3 - Group the DATA depending on the DIMENSION
     //      -   Calculates the METRICS
     //  The Y-AXIS
+    //  GROUP
     var yearGroup = yearDim.group();
 
-    console.log('Year Group');
-
+    var amountOfCrimesGroup = amountOfCrimesDim.group().reduceSum(function(d){
+        return d.amount;
+    });
 
     //  FOR PIE CHART - BY CATEGORY
     var crimeTypesGroup = crimeTypesDim.group().reduceSum(function(d){
        if(d.category=="crime"){
-       //    if(d.year="2014"){
-               console.log(d);
+
                return d.amount;
-         //  }
        }
     });
-    console.log(' print filter for crimeTypesDim: ');
-    print_filter(crimeTypesGroup);
-
+    
     //  FOR TABLE AND LINE CHART
     var justiceSysTypesGroup = justiceSystemTypesDim.group();
-    console.log('justiceSysTypesGroup GROUP: ');
-    print_filter(justiceSysTypesGroup);
 
     //  #######################################################
     //  CALCULATE TOTALS
@@ -96,17 +105,51 @@ function buildGraphs(error,jsonData){
     //  #######################################################
     //  DEFINE CHART TYPES AND BIND THEM TO DIVS IN INDEX.HTML
     //  #######################################################
+    var selectField = dc.selectMenu('#menu-select');
+   //     console.log('amountOfCrimesGroup:');
+    //print_filter(amountOfCrimesGroup);
+
+    var amountOfCrimeRowChart = dc.rowChart('#crime-level-row-chart');
+        // DEFINE VALUES TO BE USED IN SCALING THE DATA
+       // var minCrimeAmt = amountOfCrimesDim.bottom(1)(0)['amount'];
+      //  var maxCrimeAmt = amountOfCrimesDim.top(1)(0)['amount'];
+
+    console.log('amountOfCrimesGroup');
+    print_filter(amountOfCrimesGroup);
+
+
+
+
+    //var totalCrimesND = dc.numberDisplay('#total-crimes-nd');
     var crimeTypesPieChart = dc.pieChart('#crime_types');
     var justiceSystemTypesPieChart = dc.pieChart('#justice_system_types');
-    //var totalCrimesND = dc.numberDisplay('#total-crimes-nd');     //  TOTAL NUMBER OF CRIMES
+
+
 
     //  #######################################################
     //  BUILD THE CHARTS BY ASSIGNING PROPERTIES AND VALUES
     //  #######################################################
-    //  build the SELECT MENU
-    selectField = dc.selectMenu('#menu-select')
-                    .dimension(yearDim)
-                    .group(yearGroup);
+    //  CREATE A FILTER BASED ON THE SELECTION
+    selectField.dimension(yearDim)
+                .group(yearGroup)
+                .title(function(d){
+                    return d.key;
+                })
+                .filter(function(d){
+                    var value = $(this).find(':selected').val();
+                    return value;
+                });
+
+
+    //  ROW CHART
+    amountOfCrimeRowChart
+        .width(600)
+        .height(400)
+        .x(d3.scale.linear().domain([0,50]))
+        .elasticX(true)
+        .dimension(amountOfCrimesDim)
+        .group(amountOfCrimesGroup)
+        .xAxis().ticks(10);
 
     //  FORMAT Numbers to be displayed in numberDisplay
     /*totalCrimesND
@@ -118,12 +161,17 @@ function buildGraphs(error,jsonData){
         .formatNumber(d3.format(".3s"));*/
 
     crimeTypesPieChart
-        .width(300)
-        .radius(90)
+        .width(400)
+        .height(400)
+        .radius(130)
         .innerRadius(40)
+        .externalLabels(30)
         .transitionDuration(1500)
         .dimension(crimeTypesDim)
-        .group(crimeTypesGroup);
+        .group(crimeTypesGroup)
+        .legend();
+
+
 
     justiceSystemTypesPieChart
         .width(300)
@@ -132,6 +180,10 @@ function buildGraphs(error,jsonData){
         .transitionDuration(1500)
         .dimension(justiceSystemTypesDim)
         .group(justiceSysTypesGroup);
+
+
+
+
 
     //  #######################################################
     //  RENDER THE CHARTS
